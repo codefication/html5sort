c7fd0f334f51f4033398d5258acd629d375d7119
seemahemani <seema.hemani@accionlabs.com>
2015-05-14 11:03:12 +0530

    html5 sort

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.sortable').sortable();
+  $('.handles').sortable({
+    handle: 'span'
+  });
+  $('.connected').sortable({
+    connectWith: '.connected'
+  });
+  $('.exclude').sortable({
+    items: ':not(.disabled)'
+  });
+});
